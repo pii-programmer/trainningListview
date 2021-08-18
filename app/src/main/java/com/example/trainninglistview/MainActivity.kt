6d1244f8b316a21d04ec1e58380f114b3591bccf
pii-programmer <pii.programmer@gmail.com>
2021-08-18 12:19:03 +0900

    リストデータを作る

--- app/src/main/java/com/example/trainninglistview/MainActivity.kt
+++ app/src/main/java/com/example/trainninglistview/MainActivity.kt
@@ -10,6 +10,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // リストデータを作る
+        val dataList = arrayListOf<Data>()
+        for (i in 0..100){
+            dataList.add(Data().apply {
+                title = "${i}番目のタイトル"
+                text = "${i}番目のテキスト"
+            })
+        }
 
         // アダプターをセットする
         val adapter = CustomAdapter(this, dataList)
