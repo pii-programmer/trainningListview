a2d8ee417b0173181de0e31f2415b2ac214ca806
pii-programmer <pii.programmer@gmail.com>
2021-08-18 11:39:38 +0900

    一行分のDataを取得する

--- app/src/main/java/com/example/trainninglistview/CustomAdapter.kt
+++ app/src/main/java/com/example/trainninglistview/CustomAdapter.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ImageView
+import android.widget.TextView
 
 class CustomAdapter(context: Context, list:ArrayList<ContactsContract.Data>):
     ArrayAdapter<Data>(context,0,list){
@@ -17,6 +18,12 @@ class CustomAdapter(context: Context, list:ArrayList<ContactsContract.Data>):
         if (view == null){
             view = LayoutInflater.from(context).inflate(R.layout.row_view, parent,false)
         }
+
+        data = getItem(position) as Data
+        view?.findViewById<ImageView>(R.id.icon).apply{data.icon}
+        view?.findViewById<TextView>(R.id.title)?.apply { text = data.title }
+        view?.findViewById<TextView>(R.id.text)?.apply { text = data.text}
+
     }
 }
 
