1183c6881aee14541820cd0bd8c04ba038b0982a
pii-programmer <pii.programmer@gmail.com>
2021-08-18 11:35:04 +0900

    もしDataが何もない状態なら一行分のレイアウトを表示する

--- app/src/main/java/com/example/trainninglistview/CustomAdapter.kt
+++ app/src/main/java/com/example/trainninglistview/CustomAdapter.kt
@@ -1,9 +1,24 @@
 package com.example.trainninglistview
 
 
+import android.content.Context
+import android.provider.ContactsContract
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.ImageView
 
-
+class CustomAdapter(context: Context, list:ArrayList<ContactsContract.Data>):
+    ArrayAdapter<Data>(context,0,list){
+    private lateinit var data:Data
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var view = convertView
+        if (view == null){
+            view = LayoutInflater.from(context).inflate(R.layout.row_view, parent,false)
+        }
+    }
+}
 
 
 data class Data(
