d1248a0e4cd802de8ff8bccb9fa8ad778e617359
pii-programmer <pii.programmer@gmail.com>
2021-08-18 11:31:01 +0900

    CustomAdapterにDataクラスを作成

---
+++ app/src/main/java/com/example/trainninglistview/CustomAdapter.kt
@@ -0,0 +1,13 @@
+package com.example.trainninglistview
+
+
+import android.widget.ImageView
+
+
+
+
+data class Data(
+    var icon: ImageView? = null,
+    var title: String? = null,
+    var text: String = null
+)
