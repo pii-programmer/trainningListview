d882d6731505fc220cc6355a1d15b6b7afa27832
pii-programmer <pii.programmer@gmail.com>
2021-08-19 15:57:24 +0900

    画面遷移のためにsubActivityを作成

---
+++ app/src/main/java/com/example/trainninglistview/SubActivity.kt
@@ -0,0 +1,12 @@
+package com.example.trainninglistview
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SubActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sub)
+    }
+}
