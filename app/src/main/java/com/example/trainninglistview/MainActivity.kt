5248ba3f193e692593dcc7668c2a1f67257ccc36
pii-programmer <pii.programmer@gmail.com>
2021-08-19 13:03:27 +0900

    materialをimport可能にするためのdependenceを追加

--- app/src/main/java/com/example/trainninglistview/MainActivity.kt
+++ app/src/main/java/com/example/trainninglistview/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.trainninglistview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
