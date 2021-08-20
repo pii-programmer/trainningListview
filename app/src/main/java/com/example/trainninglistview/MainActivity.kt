c20879aa40de8d0c4a629da0add85031160730be
pii-programmer <pii.programmer@gmail.com>
2021-08-20 11:45:04 +0900

    まずクリックした要素を取得する

--- app/src/main/java/com/example/trainninglistview/MainActivity.kt
+++ app/src/main/java/com/example/trainninglistview/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.example.trainninglistview
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,21 +15,35 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // リストデータを作る
-        val dataList = arrayListOf<Data>()
-        for (i in 0..100){
-            dataList.add(Data().apply {
-                title = "${i}番目のタイトル"
-                text = "${i}番目のテキスト"
-            })
-        }
 
-        custom_list_view.setOnClickListener {
-            
-        }
+            // リストデータを作る
+            val dataList = arrayListOf<Data>()
+            for (i in 0..100){
+                dataList.add(Data().apply {
+                    title = "${i}番目のタイトル"
+                    text = "${i}番目のテキスト"
+                })
+            }
+            // アダプターをセットする
+            val adapter = CustomAdapter(this, dataList)
+            custom_list_view.adapter = adapter
 
-        // アダプターをセットする
-        val adapter = CustomAdapter(this, dataList)
-        custom_list_view.adapter = adapter
+
+            custom_list_view.setOnItemClickListener { parent: ViewGroup, view: View, position: Int, id -> custom_list_view }
+            // intentで送る
+//            custom_list_view.setOnClickListener {
+//                val intent = Intent(this, SubActivity::class.java)
+//              intent.putExtra("USER_SELECTED_ICE", "1")
+//              startActivity(intent)
+//            }
+
+
+
+        // 画面遷移
+//        custom_list_view.setOnClickListener {
+//            val intent = Intent(this, SubActivity::class.java)
+//            intent.putExtra("USER_SELECTED_ICE", "1")
+//            startActivity(intent)
+//        }
     }
 }
--- app/src/main/java/com/example/trainninglistview/SubActivity.kt
+++ app/src/main/java/com/example/trainninglistview/SubActivity.kt
@@ -2,11 +2,22 @@ package com.example.trainninglistview
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_sub.*
 
 class SubActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sub)
+
+          // intent処理
+//        val userSelectedIce :String? = intent.getStringExtra("USER_SELECTED_ICE")
+//
+//        when (userSelectedIce) {
+//            "1" -> {
+//        user_selected_ice.setImageResource(R.drawable.ice_strawberry)
+//            }
+//        }
     }
 }
