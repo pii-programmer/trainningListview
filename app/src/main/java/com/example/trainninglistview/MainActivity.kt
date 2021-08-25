c6f313a0064c07f2bb93b6265fc25782c82cce24
pii.programmer <pii.programmer@gmail.com>
2021-08-25 18:19:16 +0900

    ラムダ式でデータを作り直した

--- app/src/main/java/com/example/trainninglistview/MainActivity.kt
+++ app/src/main/java/com/example/trainninglistview/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.trainninglistview
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -11,6 +12,7 @@ import android.view.ViewGroup
 import android.widget.*
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.row_view.*
+import kotlinx.android.synthetic.main.row_view.view.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -19,52 +21,78 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // カスタムアダプターに渡すリストデータ(Data)を作る
+        // カスタムアダプターに渡すデータリスト(Data)を作るラムダ式で
         val dataList = arrayListOf<Data>()
-
-        for (i in 0..32) {
-            for (i in 0..2) {
-                when {
-                    i == 0 -> {
-                        dataList.add(Data().apply {
-                            icon = "a"
-                            title = "最初のタイトル"
-                            text = "最初のテキスト"
-                        })
+        for (i in 0..100){
+            dataList.add(Data().apply {
+                title = "${i}番目のタイトル"
+                text = "${i}番目のテキスト"
+                when{
+                    i %3 == 0 -> {
+                        this.icon = "a"
                     }
-                    i == 1 -> {
-                        dataList.add(Data().apply {
-                            icon = "b"
-                            title = "真ん中のタイトル"
-                            text = "真ん中のテキスト"
-                        })
+                    i %2 == 0 -> {
+                        this.icon = "b"
                     }
-                    i == 2 -> {
-                        dataList.add(Data().apply {
-                            icon = "c"
-                            title = "最後のタイトル"
-                            text = "最後のテキスト"
-                        })
+                    else -> {
+                        this.icon = "c"
                     }
                 }
-            }
+            })
         }
 
         // アダプターをセットする
         val adapter = CustomAdapter(this, dataList)
         list_view.adapter = adapter
 
-
-        // title だけを Intent で送って画面遷移させてみる
-            list_view.onItemClickListener
-                    AdapterView.OnItemClickListener { parent, view, position, id ->
-
-                        val userSelectedTitle = title[position] as String
-
-                        val intent = Intent(this, SubActivity::class.java)
-
-                        intent.putExtra("USER_SELECTED_TITLE" , userSelectedTitle)
-                        startActivity(intent)
-                    }
+        // リストビューのクリックリスナーの書き方： list_view.setOnItemClickListener{adapterView, , view, position, id -> ..ここに処理..}
+//
+//        list_view.setOnItemClickListener { parent, view, position, id ->
+//            parent.getItemAtPosition(dataList.title)
+//            val intent = Intent(this, SubActivity::class.java)
+//            intent.putExtra("USER_SELECTED_LIST", "a")
+//            startActivity(intent)
+//        }
+//
+//        list_view.setOnItemClickListener { parent, view, position, id ->
+//            parent.getItemAtPosition(position)
+//            val intent = Intent(this, SubActivity::class.java)
+//            intent.putExtra("USER_SELECTED_LIST", "b")
+//            startActivity(intent)
+//        }
+//
+//        list_view.setOnItemClickListener { parent, view, position, id ->
+//            parent.getItemAtPosition(position)
+//            val intent = Intent(this, SubActivity::class.java)
+//            intent.putExtra("USER_SELECTED_LIST", "c")
+//            startActivity(intent)
+//        }
     }
 }
+
+//  トースト表示はできた
+//  Toast.makeText(applicationContext, userSelectedList.toString(), Toast.LENGTH_SHORT).show()
+
+//  クリックしたリストの位置を定義： val userSelectedList = parent.getItemAtPosition(position)
+
+
+
+/////////////////// メモ（スコープ関数） ////////////////////////////
+//        val dataList = arrayListOf<Data>().apply {
+//            var data = Data()
+//            for (i in 0..99) {
+//                data.icon = "${i} 番目のアイコン"
+//                data.title = "${i} 番目のタイトル"
+//                data.text = "${i} 番目のテキスト"
+//                this.add(data)
+//            }
+//        }
+//        print(dataList)
+
+
+// collectionメソッドを使ってみたけど同じ画像しか出せないのでコメントアウト
+//            val icon = listOf("a", "b", "c")
+//                for(n in icon){
+//                    this.icon = n
+//                }
+
--- app/src/main/java/com/example/trainninglistview/SubActivity.kt
+++ app/src/main/java/com/example/trainninglistview/SubActivity.kt
@@ -2,10 +2,12 @@ package com.example.trainninglistview
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.content.Intent
+import android.util.Log
 import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.row_view.view.*
+import kotlinx.android.synthetic.main.activity_sub.*
 
 
 class SubActivity : AppCompatActivity(){
@@ -13,9 +15,19 @@ class SubActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sub)
 
-    // title だけを Intent で受け取ってみる
-        val userSelectedIceTitle = intent.getStringExtra("USER_SELECTED_TITLE")
-        findViewById<TextView>(R.id.user_selected_title).text = userSelectedIceTitle
 
+        val getUserSelectedList = intent.getStringExtra("USER_SELECTED_LIST")
+        
+        when{
+            getUserSelectedList == "a" -> {
+                user_selected_title.text = "リストaを表示"
+            }
+            getUserSelectedList == "b" -> {
+                user_selected_title.text = "リストbを表示"
+            }
+            getUserSelectedList == "c" -> {
+                user_selected_title.text = "リストcを表示"
+            }
+        }
     }
 }
